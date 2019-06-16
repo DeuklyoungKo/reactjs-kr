84da95a37c61c63b01fb9044c0216158ea4f65d7
DeuklyoungKo <deutschgon@gmail.com>
2019-06-16 22:16:32 +0200

    chapter 14 (https://opentutorials.org/module/4058)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-
-class Content extends Component {
-  render() {
-    return (
-        <article>
-          <h2>{this.props.title}</h2>
-            {this.props.desc}
-        </article>
-    )
-  }
-}
+import TOC from './component/TOC';
+import Content from './component/Content';
+import Subject from './component/Subject';
 
 
-class TOC extends Component {
-  render() {
-    return (
-        <nav>
-          <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
-          </ul>
-        </nav>
-    )
-  }
-}
-
-class Subject extends Component {
-  render(){
-    return (
-        <header>
-          <h1>{this.props.title}</h1>
-            {this.props.sub}
-        </header>
-    );
-  }
-}
 
 class App extends Component {
   render(){
